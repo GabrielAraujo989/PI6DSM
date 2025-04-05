66f2dba63f624d434c4f5545840a88c76ba7fe5c
GabrielAraujo989 <ga.padua92@gmail.com>
2025-04-05 10:33:57 -0300

    correção de porta e tralala do banco

--- service/dist/typeorm.config.js
+++ service/dist/typeorm.config.js
@@ -5,11 +5,11 @@ const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 exports.default = new typeorm_1.DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT || '5432', 10),
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    database: process.env.DB_DATABASE || 'registration_data',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: ['src/**/*.entity.ts'],
     migrations: ['src/migrations/*.ts'],
     synchronize: false,
