1b49ca560af6020489e058240b142862428820bf
GabrielAraujo989 <ga.padua92@gmail.com>
2025-06-06 23:48:08 -0300

    ajustes de curl do service e imagem correta para teste do front em produção

--- frontend/app/api/authApi.ts
+++ frontend/app/api/authApi.ts
@@ -3,10 +3,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const login = async (email: string, password: string) => {
   const response = await api.post('/auth/login', { email, password });
-  const access_token = (response.data as any).access_token;
+  const { access_token, name, role } = response.data as any;
   console.log('JWT capturado:', access_token); // Exibe o JWT no console do navegador
+  console.log('Usuário logado:', name);
+  console.log('Role:', role);
   await AsyncStorage.setItem('token', access_token);
-  return access_token;
+  // Retorna também nome e role para uso no frontend
+  return { access_token, name, role };
 };
 
 export const logout = async () => {
